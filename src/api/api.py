096c6eff13eb51907998703a68453c948f06fc13
hamzabouzid <hamza.bouzid@wecanconsulting.it>
2023-11-17 20:26:17 +0100

    add search api

--- src/api/api.py
+++ src/api/api.py
@@ -5,6 +5,4 @@ from api.auth import basic_auth
 
 router = APIRouter()
 router.include_router(test.router, prefix="/test", tags=["test"])
-router.include_router(
-    book.router, prefix="/book", tags=["book"], dependencies=[Depends(basic_auth)]
-)
+router.include_router(book.router, prefix="/book", tags=["book"], dependencies=[Depends(basic_auth)])
